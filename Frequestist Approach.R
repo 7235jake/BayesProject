e80650123a13ba9ecafbcfe895268234406acdb5
7235jake <jlavitt22@vt.edu>
2022-05-04 13:12:53 -0400

    sampled data and Freq

---
+++ Frequestist Approach.R
@@ -0,0 +1,35 @@
+# Data
+library(MASS)
+library(readxl)
+data <- read_excel("RealEstate.xls")
+
+## Random sample of 100
+# index <- sample(c(1:350), size=100, replace =F)
+# newData <- data[c(index),]
+# save(newData, file = "newData.Rdata")
+
+load("newData.Rdata")
+
+# Naming Variables
+
+## Sale Price
+price <- newData$`Sale Price`
+logPrice <- log(price)
+
+## Age
+age <- newData$Age
+## Percent College - % in neighborhood who graduated college
+college <- newData$`Pct College`
+## # of Bedrooms
+br <- newData$Bedrooms
+## Lot size
+size <- newData$`Lot Size`
+
+fullMod <- lm(log(price) ~ age + college + br + size)
+stepMod <- stepAIC(fullMod, direction = "both", trace = 1)
+
+summary(stepMod)
+plot(stepMod)
+
+finalMod <- lm(log(price) ~ age + college + br + size)
+summary(finalMod)
